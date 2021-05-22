e66c0f44cac96e83d78539904ff8c709393500ff
appletest <appletest@xlbstudio.idv.tw>
2021-05-22 14:32:06 +0800

    project second commit

--- src/main/java/idv/xlbstudio/hello/HelloTest.java
+++ src/main/java/idv/xlbstudio/hello/HelloTest.java
@@ -3,5 +3,6 @@ package idv.xlbstudio.hello;
 public class HelloTest {
     public static void main(String[] args) {
         System.out.println("hello test!");
+        System.out.println("tomcat-hello");
     }
 }
